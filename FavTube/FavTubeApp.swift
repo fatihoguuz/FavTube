cf80b205cd65eaf2ed98e8b1d07bba9f4e50465e
Fatih Oguz <oguzfatih39@gmail.com>
2024-06-22 12:37:12 +0300

    Initial Commit

---
+++ FavTube/FavTubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavTubeApp.swift
+//  FavTube
+//
+//  Created by Fatih Oğuz on 22.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct FavTubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
